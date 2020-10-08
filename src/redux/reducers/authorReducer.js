8cc4b174dd515787afe4b39cbb941c95a30f9251
vijaymanikanta444 <vijayakp444@gmail.com>
2020-10-08 20:08:17 +0530

    structure of redux

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,7 +1,7 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function courseReducer(state = initialState.action, action) {
+export default function courseReducer(state = initialState.authors, action) {
   switch (action.type) {
     case types.LOAD_AUTHORS_SUCCESS:
       return action.authors;
